ffe24afd874421a595629ea05a7c816ade277a8b
gabin <g.rolland27@laposte.net>
2019-09-30 09:04:25 +0200

    notebook 2 and new images

--- python_files/lag_study.py
+++ python_files/lag_study.py
@@ -12,6 +12,18 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import multiprocessing as mp
+
+import sys
+
+def progress(count, total, status=''):
+    bar_len=60
+    filled_len=int(round(bar_len*count/float(total)))
+    
+    percents=round(100.0*count/float(total),1)
+    bar= '=' * filled_len+'-'*(bar_len-filled_len)
+    sys.stdout.write('\r%s |%s| %s%% %s\r' % (bar, percents, '%', status))
+    sys.stdout.flush()
 
 def json_extracter(data): #data should be a string like : 'data.json', the function returns data and events
     with open(data) as json_file:  
@@ -19,29 +31,28 @@ def json_extracter(data): #data should be a string like : 'data.json', the funct
         events=data['events']
     return(data,events)
 
-#match_data,events=json_extracter('/Volumes/My Passport/GABIN/Documents/CENTRALE_LYON_1A/PaR/Basket/0021500001.json')
+match_data,events=json_extracter('0021500001.json')
+save_DELTA_X=0
+save_DELTA_T=0
 
-def intercorrel(DELTA_X,DELTA_T,T,N=round(4/0.04)):
+def intercorrel(DELTA_X,DELTA_T,T,N=int(round(5/0.04))):
     
     RHO=np.zeros(N)
     
-    mean_DELTA_X=[[0]*94 for i in range(50)]
-    mean_DELTA_T=[[0]*94 for i in range(50)]
+    mean_DELTA_X=np.mean(DELTA_X, axis=0)
+    mean_DELTA_T=np.mean(DELTA_T, axis=0)
+    
+    delta_x_RMS=0
+    delta_t_RMS=0
 
     for n in range(50):
         for p in range(94):
-            mean_DELTA_X[n][p]=np.mean(DELTA_X[n][p])
-            mean_DELTA_T[n][p]=np.mean(DELTA_T[n][p])
+            for i in range(len(DELTA_X)):
+                delta_x_RMS+=(DELTA_X[i][n][p]-mean_DELTA_X[n][p])**2
+                delta_t_RMS+=(DELTA_T[i][n][p]-mean_DELTA_T[n][p])**2
     
-    print(mean_DELTA_X[0][0],DELTA_X[0][0])
-            
-    delta_x_RMS=0
-    delta_t_RMS=0
-    for n in range(50):
-        for p in range(94):
-            for i in range(len(DELTA_X[n][p])):
-                delta_x_RMS+=(DELTA_X[n][p][i]-mean_DELTA_X[n][p])**2
-                delta_t_RMS+=(DELTA_T[n][p][i]-mean_DELTA_T[n][p])**2
+    #print(mean_DELTA_X[0][0],DELTA_X[0][0])
+
     delta_x_RMS=np.sqrt(delta_x_RMS/(50*94*T))
     delta_t_RMS=np.sqrt(delta_t_RMS/(50*94*T))
     
@@ -49,10 +60,10 @@ def intercorrel(DELTA_X,DELTA_T,T,N=round(4/0.04)):
         for n in range(50):
             for p in range(94):
                 rho=0
-                for i in range(len(DELTA_X[n][p])-k):
-                    deltax=DELTA_X[n][p][i+k]
+                for i in range(len(DELTA_X)-k):
+                    deltax=DELTA_X[i+k][n][p]
                     mean_deltax=mean_DELTA_X[n][p]
-                    deltat=DELTA_T[n][p][i]
+                    deltat=DELTA_T[i][n][p]
                     mean_deltat=mean_DELTA_T[n][p]
                     rho+=(deltat-mean_deltat)*(deltax-mean_deltax)
                     
@@ -61,128 +72,47 @@ def intercorrel(DELTA_X,DELTA_T,T,N=round(4/0.04)):
         RHO[k]=RHO[k]/(delta_x_RMS*delta_t_RMS)
     
     return(RHO/(94*50))            
-    
 
-def event_correlation(event):
-
-    DELTA_X=[[[]]*94 for i in range(50)]
-    DELTA_T=[[[]]*94 for i in range(50)]
-    moments=event['moments']
-    T=moments[0][2]-moments[-1][2]
-    print(T)
-    
-    for k in range(len(moments)-1):
-        #if k%10==0:
-        deb=time.time()
-            #print(k)
-        moment1=moments[k]
-        moment2=moments[k+1]
-        mom_infos=sp.players_ball_speed_position(moment1,moment2)
-        for i in range(50):
-            for j in range(94):
-                
-                b=np.array([j,i])
-                
-                deltax=sp.distance_difference(mom_infos,b)
-                deltat=sp.time_difference(mom_infos,b)
-    
-                DELTA_X[i][j]=DELTA_X[i][j]+[deltax]
-                DELTA_T[i][j]=DELTA_T[i][j]+[deltat]
-        fin=time.time()
-        print(fin-deb)
-    
-    R=intercorrel(DELTA_X,DELTA_T,T)
-    
-    plt.plot(R)
-    plt.show()
-    
-    maxi=0
-    t=0
-    for k in range(len(R)):
-        if R[k]>maxi:
-            maxi=R[k]
-            t=k
-    return(maxi,t)
-
-def court_distance_map_player(x,y):
-    
-    X,Y=np.meshgrid(np.arange(94),np.arange(50))
-    court=np.sqrt((X-x)**2+(Y-y)**2)
-    return(court)
-
-def court_distance_map_team(team, mom_infos):
-    
-    distance_map_team=[]
-    
-    for player in mom_infos[team].keys():
-        x=mom_infos[team][player]['xy'][0]
-        y=mom_infos[team][player]['xy'][1]
-        distance_map_team.append(court_distance_map_player(x,y))
-        
-    return(np.array(distance_map_team)).min(axis=0)
-
-def court_time_map_player(a,v):
-    
-    court=np.zeros((50,94))
-    
+def time_diff_matrix(mom_infos):
+    global f
+    D_T=np.zeros([50,94])
     for i in range(50):
         for j in range(94):
-            
             b=np.array([j,i])
-            court[i][j]=sp.time_to_point(a,b,v)
+            D_T[i][j]=sp.time_difference(mom_infos,b,f)
+    return D_T
 
-    return(court)
+def distance_diff_matrix(mom_infos):
+    D_X=np.zeros([50,94])
+    for i in range(50):
+        for j in range(94):
+            b=np.array([j,i])
+            D_X[i][j]=sp.distance_difference(mom_infos,b)
+    return D_X
 
-def court_time_map_team(team, mom_infos):
-    
-    time_map_team=[]
-    
-    for player in mom_infos[team].keys():
-        a=mom_infos[team][player]['xy']
-        v=mom_infos[team][player]['v']
-        time_map_team.append(court_time_map_player(a,v))
-        
-    return(np.array(time_map_team)).min(axis=0)
-    
-    
-def event_correlation_bis(event):
-    
-    Maps_distance=[]
-    Maps_time=[]
-    
+def event_correlation(event,F):
+    deb=time.time()
     moments=event['moments']
     T=moments[0][2]-moments[-1][2]
     
+    #DELTA_X = np.zeros([50, 94, len(moments)-1])
+    #DELTA_T = np.zeros([50, 94, len(moments)-1])
+    MOM_INFOS=[]
     for k in range(len(moments)-1):
-        #if k%10==0:
-        deb=time.time()
-            #print(k)
-            
-        # recuperying moment information we need
         moment1=moments[k]
         moment2=moments[k+1]
-        mom_infos=sp.players_ball_speed_position(moment1,moment2)
-        
-        #calculation of court distance map for both TEAM 
-        TEAM1_map_distance=court_distance_map_team('team1', mom_infos)
-        TEAM2_map_distance=court_distance_map_team('team2', mom_infos)
-        
-        Maps_distance.append(TEAM1_map_distance-TEAM2_map_distance)
-        
-        #calculation of time distance map for both TEAM 
-        
-        TEAM1_map_time=court_time_map_team('team1', mom_infos)
-        TEAM2_map_time=court_time_map_team('team2', mom_infos)
-        
-        Maps_time.append(TEAM1_map_time-TEAM2_map_time)
-        
-        fin=time.time()
-        print(fin-deb)
-        
-    R=intercorrel(Maps_distance,Maps_time,T)
+        MOM_INFOS.append(sp.players_ball_speed_position(moment1,moment2))
     
-    plt.plot(R)
-    plt.show()
+    pool=mp.Pool()
+    DELTA_X=pool.map(distance_diff_matrix, MOM_INFOS)
+    DELTA_T=pool.map(time_diff_matrix, MOM_INFOS)
+    pool.close()
+    pool.join()        
+
+    R=intercorrel(DELTA_X,DELTA_T,T)
+    
+    #plt.plot(R)
+    #plt.show()
     
     maxi=0
     t=0
@@ -190,13 +120,32 @@ def event_correlation_bis(event):
         if R[k]>maxi:
             maxi=R[k]
             t=k
-    return(maxi,t)
-        
+    return(R,maxi,t)
+    
+
+def match_correlation(events):
+    correlations=[]
+    times=[]
+    tau=[]
+    rho=[]
+    for event in events:
+        print(event['eventId'])
+        moments=event['moments']
+        T=moments[0][2]-moments[-1][2]
+        if T>6:
+            times.append(T)
+            R,maxi,t=event_correlation(event)
+            correlations.append(R)
+            tau.append(t)
+            rho.append(maxi)
+    
+    df=pd.DataFrame({'tau':tau,'maxi':rho,'time':times})
+    plt.plot(correlations)
+    df.to_csv('match_correl.csv')
+    df2=pd.DataFrame({'correl':correlations})
+    df2.to_csv('match_correl2.csv')
         
 
-def match_dataframe(events):
-    for event in events:
-        print(event)
         
         
 def dt_event(i):
@@ -208,7 +157,91 @@ def dt_event(i):
     
     T=pd.DataFrame({'time':T})
     print(T['time'].value_counts())
+    
+
+frames=[]
+forces=[i for i in range(1,20)]+[30,50,100,1000]
+keys=[]
+moments_before=[]
+if __name__=='__main__':
+    for event in events :
+        moments=event['moments']
+        if len(moments_before)!=len(moments):
+            moments_before=moments
+            T=moments[0][2]-moments[-1][2]
+            if T>6:
+                print(event['eventId'])
+                keys.append(event['eventId'])
+                progress(0, len(forces), status=str(0))
+                frames_forces=[]
+                for i in range(len(forces)) :
+                    f=forces[i]
+                    R,maxi,t=event_correlation(event,f)
+                    df=pd.DataFrame({'R':R,'tau':t,'rho':maxi,'time':T})
+                    frames_forces.append(df)
+                    progress(i, len(forces), status=str(f))
+                event_df=pd.concat(frames_forces,keys=forces)
+                frames.append(event_df)
+                event_df.to_csv('event_'+str(event['eventId'])+'.csv', sep=',', encoding='utf-8')
+            
+total_df=pd.concat(frames,keys=keys)
+total_df.to_csv('total_df.csv', sep=',', encoding='utf-8')
 
 
-#sp.print_court_teams_occupation_inertia(events,1,300)
-#sp.print_court_teams_occupation(events,1,300+18,value=True)
+def old_event_correlation(event,F):
+    deb=time.time()
+    moments=event['moments']
+    T=moments[0][2]-moments[-1][2]
+    
+    DELTA_X = np.zeros([50, 94, len(moments)-1])
+    DELTA_T = np.zeros([50, 94, len(moments)-1])
+    
+    for k in range(len(moments)-1):
+        moment1=moments[k]
+        moment2=moments[k+1]
+        mom_infos=sp.players_ball_speed_position(moment1,moment2)
+        print(mom_infos)
+        
+        for i in range(50):
+            for j in range(94):
+                b=np.array([j,i])
+                
+                deltax=sp.distance_difference(mom_infos,b)
+                deltat=sp.time_difference(mom_infos,b,F)
+    
+                DELTA_X[i][j][k]=deltax
+                DELTA_T[i][j][k]=deltat
+        
+    print(time.time()-deb)
+    
+    R=intercorrel(DELTA_X,DELTA_T,T)
+    
+    #plt.plot(R)
+    #plt.show()
+    
+    maxi=0
+    t=0
+    for k in range(len(R)):
+        if R[k]>maxi:
+            maxi=R[k]
+            t=k
+    print(time.time()-deb)
+    return(R,maxi,t)
+#            
+#        
+#for i in range(len(frames_forces)):
+#    plt.plot(frames_forces[i].R,label=str(i))
+#plt.legend()
+#    
+#TAU=[]
+#for k in range(len(forces)):
+#    TAU.append(frames_forces[k].iloc[0]['tau'])
+#plt.plot(forces,TAU)
+#
+#RHO=[]
+#for k in range(len(forces)):
+#    RHO.append(frames_forces[k].rho)
+#plt.plot(forces,RHO)
+
+    
+
